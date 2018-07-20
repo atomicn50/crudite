f07cf9f46a8c2dd5a736f91ef99d86af1c0a55f9
Tinh <tinhkle@Doxie.local>
2018-07-20 11:52:32 -0700

    added event listener to retrieve data

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
   //attach event listener to button
   //
-  $('.store-btn').on('click', function() {
-  	console.log(event.target);
-    localStorage.setItem("hrext", 'tinh')
+  $('.store-btn').on('click', function(event) {
+  	let titleValue = $('.input-field-title').val();
+  	let contentValue = $('.input-field-body').val();
+
+    localStorage.setItem('titleValue', titleValue);
+    localStorage.setItem('contentValue', contentValue);
   });
 
-  $('.get-btn').on('click', function() {
-    console.log(localStorage.getItem("hrext"));
+  $('.get-btn').on('click', function(event) {
+    // console.log(localStorage.getItem("hrext"));
+    let titleValue = localStorage.getItem('titleValue');
+    let contentValue = localStorage.getItem('contentValue');
+
+    $('.debug').html(`<p>${titleValue} ${contentValue}</p>`);
+    //using backticks so you can evaluate javascript statements inside
   });
 
   $('.delete-btn').on('click', function() {
-  	localStorage.removeItem("hrext");
+  	console.log(localStorage.removeItem("hrext"));
   });
 
 
